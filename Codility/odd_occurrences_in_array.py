a807ab7c26a2dbe32762a8c5a656a66dd4819b28
Esther Baek <esther.baek5@gmail.com>
2019-10-27 11:02:27 -0400

    codility problems

---
+++ Codility/odd_occurrences_in_array.py
@@ -0,0 +1,20 @@
+# A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+# For example, in array A such that:
+
+#   A[0] = 9  A[1] = 3  A[2] = 9
+#   A[3] = 3  A[4] = 9  A[5] = 7
+#   A[6] = 9
+# the elements at indexes 0 and 2 have value 9,
+# the elements at indexes 1 and 3 have value 3,
+# the elements at indexes 4 and 6 have value 9,
+# the element at index 5 has value 7 and is unpaired.
+
+
+from collections import Counter
+
+def solution(A):
+    counterA = Counter(A)
+
+    for num, count in counterA.items():
+        if count % 2 == 1: return num
